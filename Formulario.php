ae5a4fe532d6d317444c90874730ba4628dd3ceb
Jose-Guilherme-rocha <58530981+Jose-Guilherme-rocha@users.noreply.github.com>
2019-12-11 10:28:56 -0300

    Add files via upload

---
+++ Formulario.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Formulario_PHP</title>
+</head>
+<body>
+    <pre>
+<?php
+print_r($_POST);
+
+    $nome = $_POST["nome"];
+
+    echo "Meu nome " . $nome;
+?>    
+    </pre>
+</body>
+</html>
